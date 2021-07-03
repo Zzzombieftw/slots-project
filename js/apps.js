98a693bb18bb6fbc4d1c7e490a136aa114006738
Zzzombieftw <zahir.spencer@yahoo.com>
2021-07-03 12:04:22 -0400

    started

---
+++ js/apps.js
@@ -0,0 +1,34 @@
+// --------const------------
+
+
+
+
+
+
+
+
+
+// --------variables-------------
+const reel1 = document.getElementById("#slot1")
+const reel2 = document.getElementById("#slot2")
+const reel3 = document.getElementById("#slot3")
+
+
+
+// ---------------eventlistners----------
+
+
+
+
+
+// functions
+function startlever() {
+    
+}
+function render() {
+    
+}
+
+function spinnWheel(params) {
+    
+}
